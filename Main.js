605fc4f5ccee498e09778baa10bdffe11b114e66
Luke Palmer <lrpalmer@gmail.com>
2016-06-28 23:23:47 -0700

    Remove spirit of the forest... for now.

--- Main.js
+++ Main.js
@@ -13,23 +13,6 @@ MainModule = function(deps) {
         this.pause = pause;
 
         var field = this.field;
-
-        this.mouseActive = false;
-        this.mouseX = 0;
-        this.mouseY = 0;
-
-        var self = this;
-        var updateMouse = function (e) {
-            var bound = canvas.getBoundingClientRect();
-            self.mouseX = e.clientX - bound.left;
-            self.mouseY = e.clientY - bound.top;
-            self.mouseActive = true;
-        };
-        canvas.addEventListener('mousemove', updateMouse, false);
-        canvas.addEventListener('mouseleave', function (e) {
-            self.mouseActive = false
-        }, false);
-
     };
 
     $$.Main.prototype.mainLoop = function () {
@@ -50,42 +33,20 @@ MainModule = function(deps) {
         var plant = new Plant.Plant(newIx[0], newIx[1], dna, [255, 210, 150], 0);
         self.field.put(plant);
 
-        (function() {
-            for (var i = 0; i < 20000; i++) {
-                var x = Math.floor(Math.random() * self.field.width);
-                var y = Math.floor(Math.random() * self.field.height);
+        for (var i = 0; i < 20000; i++) {
+            var x = Math.floor(Math.random() * self.field.width);
+            var y = Math.floor(Math.random() * self.field.height);
 
-                var action = self.field.dequeueAction();
-                if (action) {
-                    x = action[0];
-                    y = action[1];
-                }
-
-                var life = self.field.get(x, y);
-                if (life) {
-                    life.run(self.field);
-                }
+            var action = self.field.dequeueAction();
+            if (action) {
+                x = action[0];
+                y = action[1];
             }
-        })();
 
-        (function() {
-            if (self.mouseActive) {
-                var spiritOfTheForestRadius = 25;
-                for (var i = 0; i < 100; i++) {
-                    var x0 = self.mouseX;
-                    var y0 = self.mouseY;
-                    var r = -spiritOfTheForestRadius*Math.log(Math.random());
-                    if (r == Infinity) continue;  // because log(0) might happen once in a blue moon.
-
-                    var theta = Math.random()*2*Math.PI;
-                    var x = x0 + Math.round(r*Math.cos(theta));
-                    var y = y0 + Math.round(r*Math.sin(theta));
-                    var life = self.field.get(x, y);
-                    if (life) {
-                        life.run(self.field);
-                    }
-                }
+            var life = self.field.get(x, y);
+            if (life) {
+                life.run(self.field);
             }
-        })();
+        }
     };
 };
